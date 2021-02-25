0eb6dd18a43d94eb4fd7f7acf342c82d81f292d4
Clayton Silva <clayton3660@gmail.com>
2021-02-25 00:50:53 -0300

    Correcoes ex01 e finalizacao ex02

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,15 +3,29 @@ function contar() {
   fim = document.getElementById('numeroF');
   res = document.getElementById('res');
   passo = document.getElementById('passo');
-  if (inicio.value == 0 || fim.value == 0 || inicio.value > fim.value) {
-    alert(`Impossível efetuar o cálculo!`);
+  if (inicio.value == 0 || fim.value == 0) {
+    //alert(`Impossível efetuar o cálculo!`);
+    res.innerHTML = 'Ipossível contar!';
+  } else {
+    res.innerHTML = 'Contando: ';
+    I = Number(inicio.value);
+    F = Number(fim.value);
+    P = Number(passo.value);
+    if (P <= 0) {
+      alert('Passo inválido! Considerando PASSO 1');
+      P = Number(1);
+    }
+    if (I < F) {
+      while (I <= F) {
+        res.innerHTML += `${I} &#128073;`;
+        I += P;
+      }
+    } else {
+      while (I >= F) {
+        res.innerHTML += `${I} &#128073;`;
+        I -= P;
+      }
+    }
+    res.innerHTML += '&#127937';
   }
-  I = Number(inicio.value);
-  F = fim.value;
-  P = Number(passo.value);
-  do {
-    res.innerHTML += `${I} &#128073;`;
-    I += P;
-  } while (I < F);
-  res.innerHTML += '&#127937';
 }
---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,22 @@
+function tabuada() {
+  numero = document.getElementById('numero');
+  //res = document.getElementById('res');
+  tab = document.getElementById('seltab');
+  num = numero.value;
+
+  if (num == '') {
+    alert('Por favor digite um número');
+  } else {
+    res.innerHTML = '';
+    tab.innerHTML = '';
+    for (c = 0; c <= 10; c++) {
+      item = document.createElement('option');
+      item.text = `${num} x ${c} = ${num * c}`;
+      item.value = `tab${c}`;
+      tab.appendChild(item);
+
+      //total = num * c;
+      //res.innerHTML += `${num} X ${c} = ${total} <br>`;
+    }
+  }
+}
